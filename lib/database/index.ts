0329f9c36f1f9d637998446aadfa6e2537e35709
Araya Musawwah <miramadhan354@gmail.com>
2024-05-26 17:26:33 +0700

    dev 2.2 - fix:Cant write to database

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -2,21 +2,23 @@
 
 import mongoose from 'mongoose'
 
-const uri = process.env.MONGODB_URI
+const MONGODB_URI = process.env.MONGODB_URI
+
 let cached = (global as any).mongoose || { conn: null, promise: null }
+
 export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn
 
-  if (!uri) throw new Error('MongoDB URI not found')
+  if (!MONGODB_URI) throw new Error('MONGODB_URI is missing')
 
   cached.promise =
     cached.promise ||
-    mongoose.connect(uri, {
+    mongoose.connect(MONGODB_URI, {
       dbName: 'eventhesizer',
       bufferCommands: false
     })
 
   cached.conn = await cached.promise
-  console.log('connected to DB')
+
   return cached.conn
 }
